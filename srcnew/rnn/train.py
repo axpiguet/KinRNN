24c15b0814eecf9316821edbe6196c9a9598b6c2
Axelle Piguet <44366648+axpiguet@users.noreply.github.com>
2023-03-27 17:42:17 +0200

    Update train.py

--- srcnew/rnn/train.py
+++ srcnew/rnn/train.py
@@ -55,13 +55,9 @@ def train_epoch(net: nn.Module, device: torch.device, dataloader: DataLoader, cr
         print(output)
         lossang  = 1*criterion(output[:,:,0:2], label_batch[:,:,0:2])
         lossemg  = criterion(output[:,:,2:9], label_batch[:,:,2:9])
-        alpha =0.1#0.1#1
-        beta =1#1
-        #if (lossemg>3*lossang  ):
-        #    alpha = 0.1
+        alpha =0.1
+        beta =1
         loss = alpha*criterion(output[:,:,0:2],label_batch[:,:,0:2])+ beta*criterion(output[:,:,2:9], label_batch[:,:,2:9])#criterion(output, label_batch)
-
-        #loss =  torch.mean(torch.sum((output[:,:,2:9]- label_batch[:,:,2:9])**2))/4#beta*criterion(torch.permute(output[:,:,2:9], (1, 2, 0)), torch.permute(label_batch[:,:,2:9], (1, 2, 0)))
 
         print('angle loss : ' , int(lossang.clone().item()))
         print('emg loss   : ' ,int(lossemg.clone().item()))
@@ -78,14 +74,12 @@ def train_epoch(net: nn.Module, device: torch.device, dataloader: DataLoader, cr
         if beta_FR != None:
             loss+= beta_FR * regul_FR(activity)
 
-        #with torch.autograd.profiler.profile() as prof:
+       
         loss.backward()
-        #print(prof.key_averages().table(sort_by="self_cpu_time_total"))
         clipping_value = 1 # arbitrary value of your choosing
         nn.utils.clip_grad_norm_(net.parameters(), max_norm=1200)
 
-        optimizer.step()    # Does the update
-        #print(net.get_weightsgrad())
+        optimizer.step()   
         batch_losses.append(loss.item())
 
     running_loss = np.mean(batch_losses)
@@ -101,8 +95,6 @@ def test_epoch(net: nn.Module, device: torch.device, dataloader: DataLoader, cri
         input_batch = torch.fliplr(torch.rot90(input_batch, k=-1)).to(device, non_blocking=True)
         label_batch = torch.fliplr(torch.rot90(label_batch, k=-1)).to(device, non_blocking=True).float()
 
-
-        #print('TEST       ' , np.shape(input_batch))
         output, activity = net(input_batch)
 
 
@@ -121,7 +113,6 @@ def test_epoch(net: nn.Module, device: torch.device, dataloader: DataLoader, cri
             output = torch.cat((output, activity), 2)# anlges then emg
 
         loss = criterion(output[:,:,0:9], label_batch[:,:,0:9])
-        #loss = criterion(output, label_batch)
         if beta1 != None: loss += beta1 * regul_L1(net)
         if beta2 != None: loss += beta2 * regul_L2(net)
         if beta_FR != None: loss+= beta_FR * regul_FR(activity)
@@ -136,7 +127,7 @@ def test_epoch(net: nn.Module, device: torch.device, dataloader: DataLoader, cri
                 plt.plot(np.linspace(0,1000,label_batch.shape[0]) , output.detach().cpu()[:,image ,1],label = 'pred Knee', color = 'orchid', linewidth=1)
                 plt.ylim([-20,50])
                 plt.legend()
-                #plt.savefig("test_kin/trial_"+str(count)+".png")
+                
                 ax_col = '#585759'
                 fig, ax = plt.subplots(1, figsize=(13, 4),frameon = False)
                 plt.plot(np.linspace(0,1000,label_batch.shape[0]), output.detach().cpu()[:,image ,1],linewidth=3, color = '#19D3C5', label ='Prediction - Knee')
@@ -172,9 +163,7 @@ def test_epoch(net: nn.Module, device: torch.device, dataloader: DataLoader, cri
                     ax[m].set_ylim([-50,50])
                     ax[m].tick_params(axis='both', colors = ax_col)
                 ax[m].set_xlabel('Time [ms]',color = ax_col)
-                #plt.plot(np.linspace(0,1000,label_batch.shape[0]) , net.get_inBiomech(activity).detach().cpu()[:,image,:], linewidth=1)
-                #ax[0].plot(np.linspace(0,1000,input_batch.shape[0]) , input_batch.cpu()[:,image,:], linewidth=1, color='b')
-                #print(torch.mean(torch.abs(input_batch.cpu()[:,image,:])))
+
                 ax[0].legend(loc = 'upper right',frameon = False)
                 plt.savefig("test_kin/intermediate_"+str(count)+".png", transparent = True)
                 fig, ax = plt.subplots(1, 1)
@@ -204,7 +193,7 @@ def cyclical_lr(stepsize, min_lr=3e-4, max_lr=3e-3):
 
     return lr_lambda
 
-#, input_names: List[str]
+
 def train_emg(net: nn.Module, ID: str, training_sets: torch.Tensor=None, training_targets: torch.Tensor=None,
         n_iterations: int=5000, beta1: float=None, beta2: float=None, beta_FR: float=None,
             config: str='main', perc_reduction: float=0.5, alpha_init=0.2, try_checkpoint: bool=False, multiprocessing: bool=False , BATCHS = 1)->None:
@@ -232,10 +221,10 @@ def train_emg(net: nn.Module, ID: str, training_sets: torch.Tensor=None, trainin
     device = torch.device(dev)
     print("Using device ", dev)
 
-    end_lr = 0.01#0.01
-    start_lr =0.01#0.01      #1
+    end_lr = 0.01
+    start_lr =0.01
     lr_find_epochs = n_iterations
-    a = 1 # number per batch ??
+    a = 1 
 
     if config!='main': net.__init__(net.input_size, net.hidden_size, net.output_size, alpha_init)
     net.to(device)
@@ -246,12 +235,8 @@ def train_emg(net: nn.Module, ID: str, training_sets: torch.Tensor=None, trainin
 
     # Use Adam optimizer
     criterion = nn.MSELoss()#reduction = 'sum')
-    optimizer = optim.Adam(net.parameters(), lr = start_lr)#lr=LR[1] if config=='main' else LR[0])
-
+    optimizer = optim.Adam(net.parameters(), lr = start_lr)
     lr_lambda = lambda x: math.exp(x * math.log(end_lr / start_lr) / (lr_find_epochs * a))+ 100*math.exp(4*x * math.log(start_lr / end_lr) / (lr_find_epochs * a))
-
-    #lr_lambda = cyclical_lr(5, min_lr=start_lr, max_lr=end_lr)
-    #scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda)
 
     training_losses = []
     testing_losses = []
@@ -294,7 +279,6 @@ def train_emg(net: nn.Module, ID: str, training_sets: torch.Tensor=None, trainin
     if config=='main':
         training_sets, training_targets = load_data('train', ID)
         testing_sets, testing_targets = load_data('test', ID)
-        #print(training_sets.shape, training_targets.shape)
         test = TensorDataset(testing_sets, testing_targets)
         test_loader = DataLoader(test, batch_size=BATCHS if config=='main' else BATCHS, shuffle=True)
     train = TensorDataset(training_sets, training_targets)
@@ -306,14 +290,10 @@ def train_emg(net: nn.Module, ID: str, training_sets: torch.Tensor=None, trainin
         running_loss = train_epoch(net, device, train_loader, criterion, optimizer, beta1=beta1, beta2=beta2, beta_FR=beta_FR, kin = True)
         #scheduler.step()
         lr_step = optimizer.state_dict()["param_groups"][0]["lr"]
-        print(lr_step)
+
         lr_find_lr.append(lr_step)
         training_losses.append(running_loss)
-        #if config=='main':
-            #print('B')
-            #testing_loss = test_epoch(net, device, test_loader, criterion, beta1=beta1, beta2=beta2, beta_FR=beta_FR)
-            #testing_losses.append(testing_loss)
-        #print('C')
+
         if epoch % print_step == 0: display_net_state(optimizer, running_loss, epoch)#, testing_loss if config=='main' else None)
         if epoch % print_step == 0:
             pred, activity = net(torch.fliplr(torch.rot90(testing_sets, k=-1)).to(device, non_blocking=True))
@@ -329,31 +309,29 @@ def train_emg(net: nn.Module, ID: str, training_sets: torch.Tensor=None, trainin
         if epoch==0: ref_loss=running_loss
         # narrow the input if loss decreased enough
         elif epoch>0 and step+1<n_step and running_loss<=ref_loss*(1-perc_reduction):
-            #print('D')
+     
             step+=1
             train = TensorDataset(training_sets, training_targets)
             train_loader = DataLoader(train, batch_size=BATCHS if config=='main' else BATCHS, shuffle=True)
             if config=='main':
-                #print('E')
+             
                 test = TensorDataset(testing_sets, testing_targets)
                 test_loader = DataLoader(test, batch_size=BATCHS if config=='main' else BATCHS, shuffle=True)
             ref_loss = running_loss
-        #print('F')
+  
         if epoch % checkpoint_step == 0 and epoch>0:
             dump_checkpoint({'epoch': epoch,'model_state': net.state_dict(),'optimizer_state': optimizer.state_dict(),'training_losses': training_losses, 'testing_losses':testing_losses, 'ref_loss': ref_loss, 'input_step': step}, config, ID)
-        #if epoch % plot_step == 0 and epoch>0 and config=='main': plot(net, training_sets[:,step,:,:], training_targets, input_names, reverse_configs, device, ID, training_config=config, epoch=epoch)
         if torch.cuda.is_available(): torch.cuda.empty_cache()
-    #print('G')
+
     # save loss and model
 
     ##### comment this line if don't want to save for pretraining
-     #net.save_pretrain()
+    #net.save_pretrain()
 
     fig, ax = plt.subplots(2, 1)
     ax[0].plot(np.linspace(0,1000,len(training_losses)) ,training_losses, linewidth=1)
-    #ax[1].plot(np.linspace(0,1000,len(training_losses)) ,lr_find_lr, linewidth=1)
-    #ax[0].set_ylim( [0,8])
-    plt.savefig("losssss.png")
+
+    plt.savefig("loss.png")
 
     dump_checkpoint({'epoch': n_iterations-1,'model_state': net.state_dict(),'optimizer_state': optimizer.state_dict(),'training_losses': training_losses, 'testing_losses':testing_losses, 'ref_loss': ref_loss, 'input_step': step}, config, ID)
     dump_loss(training_losses, 'train', config, ID)
@@ -425,7 +403,6 @@ def train(net: nn.Module, input_names: List[str], reverse_configs: np.ndarray, I
     if config=='main':
         training_sets, training_targets = load_data('train', ID)
         testing_sets, testing_targets = load_data('test', ID)
-        #print(training_sets.shape, training_targets.shape)
         test = TensorDataset(testing_sets[:,step,:,:], testing_targets)
         test_loader = DataLoader(test, batch_size=BATCH_SIZE[1] if config=='main' else BATCH_SIZE[0], shuffle=True)
     train = TensorDataset(training_sets[:,step,:,:], training_targets)
@@ -452,14 +429,12 @@ def train(net: nn.Module, input_names: List[str], reverse_configs: np.ndarray, I
             ref_loss = running_loss
         if epoch % checkpoint_step == 0 and epoch>0:
             dump_checkpoint({'epoch': epoch,'model_state': net.state_dict(),'optimizer_state': optimizer.state_dict(),'training_losses': training_losses, 'testing_losses':testing_losses, 'ref_loss': ref_loss, 'input_step': step}, config, ID)
-        #if epoch % plot_step == 0 and epoch>0 and config=='main': plot(net, training_sets[:,step,:,:], training_targets, input_names, reverse_configs, device, ID, training_config=config, epoch=epoch)
         if torch.cuda.is_available(): torch.cuda.empty_cache()
 
     # save loss and model
     dump_checkpoint({'epoch': n_iterations-1,'model_state': net.state_dict(),'optimizer_state': optimizer.state_dict(),'training_losses': training_losses, 'testing_losses':testing_losses, 'ref_loss': ref_loss, 'input_step': step}, config, ID)
     dump_loss(training_losses, 'train', config, ID)
     dump_loss(testing_losses, 'test', config, ID)
-    #plot(net, training_sets[:,step,:,:], training_targets, input_names, reverse_configs, device, ID, split='Train')
     plot_loss_trajectory(config, ID)
 
 
